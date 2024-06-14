35865e19445383630b452b6f4418333d33868078
Moxie-47 <prashants9700@gmail.com>
2024-06-14 17:10:12 +0530

    FIrst question is done: Hurrah

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,62 @@
 */
 
 function isAnagram(str1, str2) {
+let len1 = str1.length ;
+let len2 = str2.length ;
+if(len1 != len2)return false ;
+let mymap = new Map() ;
+
+
+
+for (let i = 0; i < len1; i++) {
+  // mymap[str1[index]]++ ;
+  let char = str1[i] ;
+  if(mymap.has(char)){
+    mymap.set(char , mymap.get(char) + 1) ;
+  }
+  else {
+    mymap.set(char , 1) ;
+  }
+}
+
+
+for (let i = 0; i < len1; i++) {
+
+  let char = str2[i] ;
+  if(mymap.has(char) && mymap.get(char)){
+    mymap.set(char , mymap.get(char) - 1) ;
+  }
+  else {
+    return false ;
+  }
+}
+
+// for (let index = 0; index < len2; index++) {
+//   mymap[str2[index]]-- ;
+// }
+
+for (const value of mymap.values()) {
+  if(value != 0){
+    return false ;
+  }
+}
+
+// for (let index = 0; index < len1; index++) {
+//   let char =  str1[index] ;
+//   if(mymap.get(char) != 0){
+//     return false; 
+//   }
+// }
+return true ;
 
 }
 
-module.exports = isAnagram;
+
+let ans=  isAnagram("helo" , "pelh") ;
+if(ans){
+  console.log("yes, the strings provided are anagram " , ans) ;
+  }
+  else
+console.log("No, the strings provided are not anagram " , ans) ;
+
+// module.exports = isAnagram;
